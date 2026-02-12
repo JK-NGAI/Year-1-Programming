c9b1e27a20b30b56572b89f8f3069ee3a7d23a1a
John Kariuki <johnngaij@gmail.com>
2026-02-12 11:10:43 +0300

    Add files via upload

---
+++ #practice.py
@@ -0,0 +1,7 @@
+#practice
+#introduction to python practice
+name=("John Kariuki")
+university=("\nOpen University of Kenya")
+course=("\nBSc Computer Science")
+year_of_study=("\n1.2")
+print(F"\nMy name is {name} I study {course} at {university} and I am in {year_of_study} ")
---
+++ Module 3 Mini Project.py
@@ -0,0 +1,17 @@
+# Capture input from the user
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Perform calculations
+addition = num1 + num2
+multiplication = num1 * num2
+subtraction = num1 - num2
+division = num1 / num2
+modulus = num1 % num2
+
+# Display outputs in the required order
+print(addition)
+print(multiplication)
+print(subtraction)
+print(division)
+print(modulus)
